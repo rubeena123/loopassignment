3f208301299fe23bc4790dcb772398300c16721a
rubinasatyal <rubinasatyal@gmail.com>
2017-02-07 17:05:47 -0500

    Assignmentloop

---
+++ main.js
@@ -0,0 +1,31 @@
+//0 100 200 300 400 500
+//1 2 4 8 16 32 64
+//1 1 1 2 2 2 3 3 3
+//0 2 4 6 8 10
+//3 6 9 12 15
+//9 8 7 6 5 4 3 2 1 0
+//0 1 2 3 0 1 2 3 0 1 2 3
+
+
+for(var a=0; a<=500; a+=100){
+    console.log(a); }
+for(var a=1; a<=64; a*=2){
+  console.log(a);
+}
+for (var a=1; a<=3; a++){
+  for (var b=1; b<=3; b++){
+console.log(a); }}
+for (var a=0; a<=10; a+=2){
+  console.log(a); 
+}
+for (var a=3; a<=15; a+=3){
+  console.log(a);
+}
+for (var a=9; a>=0; a-- ){
+  console.log(a); 
+}
+for (var a=0; a<=3; a++){
+ for (var b=0; b<=3; b++){
+   console.log(b);}}
+   
+  
